4d471e41ce9d69e814646afa8decebd7e6444a83
User <soumya.bhangi@gmail.com>
2022-07-21 12:27:07 +0530

    new file

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -4,5 +4,5 @@
 
 ## AMI_ID = "ami-0aa718de62aea6fbe"
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -d 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
