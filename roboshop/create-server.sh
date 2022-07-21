b6913038ea03d459efafef6833bbc6d3faaf4c4f
User <soumya.bhangi@gmail.com>
2022-07-21 14:22:14 +0530

    new file

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -8,7 +8,7 @@ SGID=sg-07448acb825353d77
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 echo "creating $COMPONENT server"
-PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIPAddress' | sed e 's/"//g')
+PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIPAddress' | sed -e 's/"//g')
 echo $PRIVATE_IP
 
 #changing ipaddress and dnsname of the component
