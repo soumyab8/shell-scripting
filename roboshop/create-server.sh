d25ada5c158479a32826421f2eda69562b550125
User <soumya.bhangi@gmail.com>
2022-07-21 13:36:27 +0530

    new file

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -4,7 +4,8 @@
 
 ## AMI_ID = "ami-0aa718de62aea6fbe"
 
+COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
-echo "creating server instance"
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq
+echo "creating $COMPONENT"
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "Resource-type=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
