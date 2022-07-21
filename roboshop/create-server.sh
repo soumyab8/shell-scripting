3dc8d242108ea6da4b9306b3021a33af1568b672
User <soumya.bhangi@gmail.com>
2022-07-21 14:33:29 +0530

    new file

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -5,6 +5,7 @@
 ## AMI_ID = "ami-0aa718de62aea6fbe"
 COMPONENT=$1
 SGID=sg-07448acb825353d77
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 echo "creating $COMPONENT server"
@@ -15,4 +16,4 @@ echo $PRIVATE_IP
 
 #changing ipaddress and dnsname of the component
 #sed -e 's/IPADDRESS/${PRIVATE_IP}'/ -e 's/COMPONENT/${COMPONENT}/' route53.json > /tmp/record.json
-#aws route53 change-resource-record-sets --hosted-zone-id Z004065932CWFU39T8EKD --change-batch file:///tmp/record.json | jq
+#aws route53 change-resource-record-sets --hosted-zone-id Z004065932CWFU39T8EKD --change-batch file:///tmp/#record.json | jq
